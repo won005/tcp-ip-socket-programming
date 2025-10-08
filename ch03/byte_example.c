d15e48a1fa50a69352ce21abc51ad6a8d71fc41d
won005 <won_8014@naver.com>
2025-10-08 23:01:03 +0900

    ch03 buffer

---
+++ ch03/byte_example.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+	//정수형
+	char xchar;   //1
+	short xshort; //2
+
+	int xint;     //4
+	long xlong;   //4
+
+	long long xxlong//8
+
+
+	//부호 없는 정수형
+	unsigned char uchar; //1
+	unsigned short ushort; //2
+
+	unsigned int uint; //4
+	unsigned long ulong;//4 or 8
+	unsigned long long uulong;//8
+
+	//실수형
+	float xfloat;//4
+	double xdouble;//8
+	long double xldouble;//8 or 12 or16
+
+	//포인터
+	// void* 	4 or 8
+}
---
+++ ch03/low_open.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+void error_handling(char* message);
+
+int main(void){
+	int fd;
+	char buf[] = "Let's go!\n"; //버퍼 배열 저장 
+	fd = open("data.txt", O_CREAT |O_WRONLY|O_TRUNC,0644);
+	if(fd==-1)
+		error_handling("open() error!");
+	printf("file descriptor: %d \n", fd);
+	
+	if (write(fd, buf, sizeof(buf))==-1)
+		error_handling("write() error!");
+	close(fd);
+	return 0;
+}
+
+void error_handling(char* message){
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
